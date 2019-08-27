cf103d566247508b0dd84e3b92052efa4136119a
Ronald Doorn <rdoorn@schubergphilis.com>
2019-08-27 16:39:19 +0200

    bugfix: check commit text for version bumps

--- tools/ci-package.sh
+++ tools/ci-package.sh
@@ -5,8 +5,8 @@ env
 
 echo "param1: $1"
 
-REF=$(git log --graph  --pretty=format:%D -1 | cut -f2 -d, | sed -e 's/.*\///g')
-echo "ref: ${REF}"
+#REF=$(git log --graph  --pretty=format:%D -1 | cut -f2 -d, | sed -e 's/.*\///g')
+#echo "ref: ${REF}"
 git describe --tags --always > .version
 echo "path: ${PWD} version: $(cat .version)"
 
@@ -15,22 +15,24 @@ if [ "${CIRCLE_BRANCH}" != "master" ]; then
     exit 0
 fi
 
+REF=$(git log -1 --pretty=%B)
+
 
 major=$(cat .version | cut -f1 -d.)
 minor=$(cat .version | cut -f2 -d.)
 patch=$(cat .version | cut -f3 -d. | cut -f1 -d-)
 rebuild=0
 case "${REF}" in
-    bug-*)
+    bugfix:*)
         patch=$((patch+1))
         rebuild=1
         ;;
-    feat-*)
+    feature:*)
         patch=0
         minor=$((minor+1))
         rebuild=1
         ;;
-    major-*)
+    major:*)
         patch=0
         minor=0
         major=$((major+1))
