7a3273e450decf94504a75cb5d91e6df3923c4cf
Ronald Doorn <rdoorn@schubergphilis.com>
2019-08-27 10:14:39 +0200

    update

--- tools/ci-package.sh
+++ tools/ci-package.sh
@@ -19,27 +19,34 @@ fi
 major=$(cat .version | cut -f1 -d.)
 minor=$(cat .version | cut -f2 -d.)
 patch=$(cat .version | cut -f3 -d. | cut -f1 -d-)
+rebuild=0
 case "${REF}" in
     bug-*)
         patch=$((patch+1))
+        rebuild=1
         ;;
     feat-*)
         patch=0
         minor=$((minor+1))
+        rebuild=1
         ;;
     major-*)
         patch=0
         minor=0
         major=$((major+1))
+        rebuild=1
         ;;
 esac
 
-echo "old version: $(cat .version) new: ${major}.${minor}.${patch}"
-echo "${major}.${minor}.${patch}" > .version
+if [ $rebuild -eq 1 ]; then
+    echo "old version: $(cat .version) new: ${major}.${minor}.${patch}"
+    echo "${major}.${minor}.${patch}" > .version
 
 
-make linux-package
+    gem install fpm
+    make linux-package
 
-go get github.com/tcnksm/ghr
-VERSION=$(cat .version)
-echo ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -delete ${VERSION} ./build/packages/
+    go get github.com/tcnksm/ghr
+    VERSION=$(cat .version)
+    echo ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -delete ${VERSION} ./build/packages/
+fi
