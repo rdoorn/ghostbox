dc8c5004b500702d3fddedbd801cc572f6230929
Ronald Doorn <rdoorn@schubergphilis.com>
2019-08-28 14:44:32 +0200

    update script

--- tools/golang-update.sh
+++ tools/golang-update.sh
@@ -1,16 +1,23 @@
 #!/bin/bash -e
 
 PREVIOUS_VERSION=$(git describe --tags --always)
-PREVIOUS_GOLANG_VERSION=$(curl https://github.com/rdoorn/ixxi/releases/download/${PREVIOUS_VERSION}/golang.version -o -)
+RESULT=$(curl -L https://github.com/rdoorn/ixxi/releases/download/${PREVIOUS_VERSION}/golang.version -o golang.version -w "%{http_code}")
+PREVIOUS_GOLANG_VERSION=$(cat golang.version)
 CURRENT_GOLANG_VERSION=$(go version)
+
+if [ "${RESULT}" != "200" ]; then
+    echo "get golang version returned status: ${RESULT}"
+    echo "url: https://github.com/rdoorn/ixxi/releases/download/${PREVIOUS_VERSION}/golang.version"
+    exit 0
+fi
 
 echo "old: [${PREVIOUS_GOLANG_VERSION}]"
 echo "new: [${CURRENT_GOLANG_VERSION}]"
 
-if [ "${CURRENT_GOLANG_VERSION}" != "${PREVIOUS_GOLANG_VERSION}" ]; then
-    echo "new golang version available, rebuilding"
-else
+if [ "${CURRENT_GOLANG_VERSION}" == "${PREVIOUS_GOLANG_VERSION}" ]; then
     echo "up to date with latest golang version"
+    exit 0
 fi
 
+echo "new golang version available, rebuilding"
 
